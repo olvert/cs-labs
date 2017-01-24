7be27885be2310e82d59d651ab3bae90bbefb71b
Olle Svensson <olle@kent.nu>
2017-01-24 18:06:10 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CS_Labs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS_Labs.dir/main.c.o"
+  "CS_Labs.pdb"
+  "CS_Labs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS_Labs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CS_Labs.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CS_Labs.
+# This may be replaced when dependencies are built.
